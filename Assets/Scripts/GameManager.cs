b971124951284bee67f45971d9c9564f6c58ec9c
Shane Moore <102933628+sMooreEWU@users.noreply.github.com>
2025-05-31 19:17:22 -0700

    Lvl-1
    Made the outline, tilemaps and sprite markers and ground work for the level
    
    -Edited the player script
    
    -Made a GameManager Script and object
    
    -Painted the 2d map with tiles
    -- added collisions to the walls(made the walls)
    -- added a lvl end room and an arrow pointing to the exit.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,61 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Place holders to allow connecting to other objects
+    public Transform spawnPoint;
+    public GameObject player;
+
+    // Flags that control the state of the game
+    private float elapsedTime = 0;
+    private bool isRunning = false;
+    private bool isFinished = false;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        //Tell Unity to allow character controllers to have their position set directly. This will enable our respawn to work
+        Physics.autoSyncTransforms = true;
+
+    }
+
+
+    //This resets to game back to the way it started
+    private void StartGame()
+    {
+        elapsedTime = 0;
+        isRunning = true;
+        isFinished = false;
+
+        // Move the player to the spawn point, and allow it to move.
+        PositionPlayer();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Add time to the clock if the game is running
+        if (isRunning)
+        {
+            elapsedTime = elapsedTime + Time.deltaTime;
+        }
+    }
+
+
+    //Runs when the player needs to be positioned back at the spawn point
+    public void PositionPlayer()
+    {
+        player.transform.position = spawnPoint.position;
+        player.transform.rotation = spawnPoint.rotation;
+    }
+
+
+    // Runs when the player enters the finish zone
+    public void FinishedGame()
+    {
+        isRunning = false;
+        isFinished = true;
+    }
+}
--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -1,16 +1,47 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerScript : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField]
+    public float moveSpeed;
+
+    private Rigidbody2D player;
+    //private GameObject player;
+    private Vector2 movement;
+    private Vector2 smoothMovement;
+    private Vector2 smoothInputVelocity;
+
+    void Awake() 
     {
-        
+        player = GetComponent<Rigidbody2D>();
+
+        //player = GameObject.FindWithTag("Player");
+    }
+
+    private void FixedUpdate()
+    {
+        smoothMovement = Vector2.SmoothDamp(smoothMovement, movement, ref smoothInputVelocity, 0.1f);
+
+        player.velocity = smoothMovement * moveSpeed;
+    }
+
+    private void OnMove(InputValue inputVal)
+    {
+        movement = inputVal.Get<Vector2>();
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    //void Update()
+    //{
+    //    float sideToSide = Input.GetAxis("Horizontal");
+    //    float upDown = Input.GetAxis("Vertical");
+
+    //    Vector3 moveVector = new Vector3(sideToSide, upDown, 0);
+    //    moveVector = moveVector.normalized * moveSpeed * Time.deltaTime;
+
+    //    player.transform.position += moveVector;
+    //player.MovePosition(player.transform.position + moveVector);
+    //}
+
 }
